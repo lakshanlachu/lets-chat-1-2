4287caa24f72bd53e9c16c4c86b78534e7c04f7e
lakshanlachu <91308981+lakshanlachu@users.noreply.github.com>
2021-11-30 09:58:48 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCQp1h2TCAZnXWGY8xRJmWGWuuP_WEmyV4",
+    authDomain: "kwiter-54ca8.firebaseapp.com",
+    databaseURL: "https://kwiter-54ca8-default-rtdb.firebaseio.com",
+    projectId: "kwiter-54ca8",
+    storageBucket: "kwiter-54ca8.appspot.com",
+    messagingSenderId: "540972482876",
+    appId: "1:540972482876:web:16f11eb7751119fcc49e39"
+  };
+ firebase.initializeApp(firebaseConfig);
